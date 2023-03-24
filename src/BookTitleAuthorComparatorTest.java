ae13695c9a948e66a57f0d9694a31f56794f17e9
AlexandrStopkin <stnalex1@gmail.com>
2023-03-24 15:33:22 +0100

    Add testMethods

--- src/BookTitleAuthorComparatorTest.java
+++ src/BookTitleAuthorComparatorTest.java
@@ -4,7 +4,37 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class BookTitleAuthorComparatorTest {
+  public BookTitleAuthorComparator bookComp = new BookTitleAuthorComparator();
 
+  @Test
+  public void AuthorsTitlesEqual() {
+    Book a = new Book("a", "b", 5);
+    Book b = new Book("a", "b", 6);
+    assertEquals(0, bookComp.compare(a, b));
+  }
 
+  @Test
+  public void AuthorsDiffTitles() {
+    Book a = new Book("a", "b", 5);
+    Book b = new Book("b", "b", 6);
+    assertTrue(bookComp.compare(a, b) < 0);
+    assertTrue(bookComp.compare(b, a) > 0);
+  }
+
+  @Test
+  public void AuthorsEqTitlesDiff() {
+    Book a = new Book("a", "a", 5);
+    Book b = new Book("a", "b", 6);
+    assertTrue(bookComp.compare(a, b) < 0);
+    assertTrue(bookComp.compare(b, a) > 0);
+  }
+
+  @Test
+  public void AuthorsDiffTitlesDiff() {
+    Book a = new Book("a", "b", 5);
+    Book b = new Book("b", "b", 6);
+    assertTrue(bookComp.compare(a, b) < 0);
+    assertTrue(bookComp.compare(b, a) > 0);
+  }
 }
 
